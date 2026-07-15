d1752ef5b8c6bcee1c5715e67be42df945f9482b
pritambaliya <pritambaliya123@gmail.com>
2026-07-15 16:13:46 +0530

    Update SameSite attribute for cookies in auth controller and route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -71,7 +71,7 @@ const logout = async (req, res) => {
     res.clearCookie("token", {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "None",
     });
 
     res.status(200).json({
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -34,7 +34,7 @@ router.get(
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
