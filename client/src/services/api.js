b270d48c3926542cfe7ddbfa3acc79a17cd062c8
pritambaliya <pritambaliya123@gmail.com>
2026-07-15 15:01:40 +0530

    Fixed axios

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -8,17 +8,4 @@ const api = axios.create({
   withCredentials: true,
 });
 
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      if (!window.location.pathname.includes("/login")) {
-        window.location.href = "/login";
-      }
-    }
-
-    return Promise.reject(error);
-  }
-);
-
 export default api;
