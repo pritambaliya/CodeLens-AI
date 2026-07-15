98c0c5642488d98b954c37ffa9578679ac807d43
pritambaliya <pritambaliya123@gmail.com>
2026-07-15 13:42:42 +0530

    Fixed baseUrl

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://codelens-ai-server.onrender.com/",
+  baseURL: "https://codelens-ai-server.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
